30d7cef5f0d1a160f6f863a3869c23ce1a88ac20
Pavel Mikhailov <62296354+e-io@users.noreply.github.com>
2020-04-29 09:33:04 +0300

    bot can greet you now

--- vk-bot.py
+++ vk-bot.py
@@ -1,3 +1,5 @@
+import random
+
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
@@ -8,3 +10,15 @@ vk_session = vk_api.VkApi(token=config.vk_token)
 longpoll = VkLongPoll(vk_session)
 
 #session_api = vk_session.get_api()
+
+while True:
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+
+            if event.from_user and not event.from_me:
+                message = f'Здравствуй пользователь @id{event.user_id}'
+                vk_session.method('messages.send',
+                                  {'user_id': event.user_id,
+                                   'message': message,
+                                   'random_id': random.random()})
+
