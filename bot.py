8939c0dc95db8ff308e87c7d8731a7d7b256c750
earthman <62296354+e-io@users.noreply.github.com>
2020-05-17 17:22:34 +0300

    bot is prepared for asynchronous programming. message_handler is separated into receiver, analyser and sender

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 # telebot is from pytelegrambotapi library
 import json
+import asyncio
+import random
 
 import telebot
 import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 class Button:
@@ -20,7 +23,7 @@ class Button:
             self.buttons = list()
             for button in dict["buttons"]:
                 self.button = Button(button["label"],
-                                button["keywords"])
+                                     button["keywords"])
                 self.buttons.add(button)
 
 
@@ -28,6 +31,17 @@ class Bot:
     """
     This is universal class for any chat-bots
     """
+
+    def add(self, type_, token, lang):
+        self.lang = lang
+
+        if type_ == "tg":
+            self.tg = telebot.TeleBot(token)
+        elif type_ == "vk":
+            self.vk = vk_api.VkApi(token=token)
+        else:
+            raise (Exception("this type of bot is not supported"))
+
     def __init__(self, type, token, lang, data):
         """
         Constructor
@@ -38,36 +52,62 @@ class Bot:
         self.tg = None
         self.vk = None
 
-        self.lang = None
-        self.start = None
         self.root = None
 
-        self.lang = lang
         self.data = json.load(open(data), encoding="utf-8")
         self.start = Button(self.data["start"])
 
-        if type == "tg":
-            self.tg = telebot.TeleBot(token)
-        elif type == "vk":
-            self.vk = vk_api.VkApi(token=token)
-            raise (Exception("vk bot is not supported now"))
-        else:
-            raise (Exception("this type of bot is not supported"))
+        self.add(type, token, lang)
+
+    def sender(self, source, id, text):
+        if source == "tg":
+            self.tg.send_message(
+                id,
+                text
+            )
+        elif source == "vk":
+            self.vk.method('messages.send',
+                           {
+                               'user_id': id,
+                               'message': text,
+                               'random_id': random.random()
+                           })
+
+    def analyzer(self, source, id, message):
+        message = message.lower()
+        for keyword in self.start.keywords[self.lang]:
+            if keyword in message:
+                self.sender(source,
+                            id,
+                            "\n".join(self.start.text[self.lang]))
+                return
+
+    async def receiver_tg(self):
+        @self.tg.message_handler(commands=['start'])
+        def commands_start(message):
+            self.sender(source,
+                        id,
+                        "\n".join(self.start.text[self.lang]))
+
+        @self.tg.message_handler(content_types=["text"])
+        def content_types_text(message):
+            self.analyzer("tg", message.chat.id, message.text)
+
+        await self.tg.polling(none_stop=True)
+
+    async def receiver_vk(self):
+        vk_longpoll = await VkLongPoll(self.vk)
+        while True:
+            for event in vk_longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.from_user and not event.from_me:
+                        await self.analyzer("vk", event.user_id, event.message)
+
+    async def receiver(self):
+        tasks = list()
+        if self.vk: tasks.append(asyncio.create_task(self.receiver_vk()))
+        if self.tg: tasks.append(asyncio.create_task(self.receiver_tg()))
+        await asyncio.gather(*tasks)
 
     def start_now(self):
-        if self.tg:
-            @self.tg.message_handler(commands=['start'])
-            def start_answer(message):
-                self.tg.send_message(
-                    message.chat.id,
-                    "\n".join(self.start.text[self.lang])
-                )
-
-            @self.tg.message_handler(content_types=["text"])
-            def answer(message):
-                for keyword in self.start.keywords[self.lang]:
-                    if keyword in message.text:
-                        start_answer(message)
-                        break
-
-            self.tg.polling(none_stop=True)
+        asyncio.run(self.receiver())
--- worker.py
+++ worker.py
@@ -1,6 +1,8 @@
 import bot
 import config
 
+
 if __name__ == "__main__":
     bot = bot.Bot("tg", config.tg_token, config.lang, "data.json")
+    bot.add("vk", config.vk_token, config.lang)
     bot.start_now()
